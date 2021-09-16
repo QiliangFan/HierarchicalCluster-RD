2bde497f528d733085620bce494de8d26936819b
liang <1035847702@qq.com>
2021-09-16 14:25:39 +0800

    update test.csv

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+import pandas as pd
+import os
+
+def main():
+    arr = pd.read_csv(os.path.join("data", ))
+
+
+if __name__ == "__main__":
+    input_csv = "test.csv"
+    main()
